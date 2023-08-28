8f963340eb10e116ac656d1a3d1830aa35d5ff99
dishyang <1209311828@qq.com>
2023-08-28 14:13:28 +0800

    项目创建

---
+++ NoProject/src/main/java/com/example/noproject/NoProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.noproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NoProjectApplication.class, args);
+    }
+
+}
---
+++ NoProject/src/test/java/com/example/noproject/NoProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.noproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
